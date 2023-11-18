e1413cda59aa66ae8afbdc02c8f5ab3cafb1fea6
vladmoroca <vladmoroca@gmail.com>
2023-11-18 15:53:07 +0200

    lab3

---
+++ lab_3/Lab3.java
@@ -0,0 +1,35 @@
+package Java_lab_2023.lab_3;
+
+import java.util.Arrays;
+
+public class Lab3 {
+    /* 
+    variant 1121
+    C3 = 2
+    String
+    С17  = 16
+    В кожному слові заданого тексту, видалити всі попередні входження останньої літери цього слова.
+    */
+
+    public static void main(String[] args) throws Exception {
+        String new_text = parse("Some text with Letters hhhhhhhth");
+        System.out.println(new_text);
+
+    }
+
+    public static String parse(String Text) {
+        String[] words = Text.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = last_letter_delete(words[i]);
+        }
+        String text = String.join(" ", words);
+        return text;
+    }
+
+    public static String last_letter_delete(String word) {
+        String lastLetter = word.substring(word.length() - 1);
+        word = word.replaceAll(lastLetter, "");
+        word += lastLetter;
+        return word;
+    }
+}
