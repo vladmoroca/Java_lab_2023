2bf86f1a952464525d9f487c857c7082492c26c5
vladmoroca <vladmoroca@gmail.com>
2023-12-05 14:58:23 +0200

    lab6 copy

---
+++ lab_7/lab7.java
@@ -0,0 +1,32 @@
+package Java_lab_2023.lab_7;
+
+import Java_lab_2023.lab_7.salat.Salat;
+import Java_lab_2023.lab_7.vegetables.*;
+
+public class lab7 {
+     /* 
+    C_2 = 1
+    Set
+    C_3 = 2
+    Двозв’язний список
+    */
+    public static void main(String[] args) throws Exception {
+        Tomat tomat = new Tomat("simple tomat", 200, 4, 3);
+        Onion onion = new Onion("simple onion", 100, 3, 3);
+        Cucumber cucumber = new Cucumber("just cucumber", 300, 5, 2);
+
+        Salat salat = new Salat();
+        salat.addVegetble(tomat);
+        salat.addVegetble(onion);
+        salat.addVegetble(cucumber);
+        System.out.println(salat.caloritySearch(50, 250));
+        System.out.println(salat.calculateCalority());
+        for (Vegetable veg : salat.getVegetables()) {
+            System.out.println(veg);
+        }
+        salat.caloritySorting();
+        for (Vegetable veg : salat.getVegetables()) {
+            System.out.println(veg);
+        }
+    }
+}
---
+++ lab_7/vegetables/Cucumber.java
@@ -0,0 +1,15 @@
+package Java_lab_2023.lab_7.vegetables;
+
+
+public class Cucumber extends Vegetable {
+    private int crunchiness;
+
+    public Cucumber(String name, int calories, int ripeness, int crunchiness) {
+        super(name, calories, ripeness);
+        this.crunchiness = crunchiness;
+    }
+
+    public int getCrunchiness(){
+        return crunchiness;
+    }
+}
